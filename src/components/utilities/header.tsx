87b668be4505e52c9e4db86aee83d533e3d498a4
james <jamesazzi193@gmail.com>
2023-10-24 21:36:45 +0300

    Adorning the look.

--- src/components/utilities/header.tsx
+++ src/components/utilities/header.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 import "../../css/header.css";
 
 function Header() {
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    var lastScrollTop = 0;
+    var reset = true;
+    const handleScroll = () => {
+      var st = window.scrollY || document.documentElement.scrollTop; 
+      if (reset && st > lastScrollTop) {
+        setIsFixed(true);
+        reset = false;
+        setTimeout(() => {
+          setIsFixed(false);
+          reset = true;
+        }, 4000);
+      } else if (st < lastScrollTop) {
+        setIsFixed(false);
+      } 
+      lastScrollTop = st <= 0 ? 0 : st
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [])
+  
   return (
-    <section className="header-block">
+    <section className={"header-block"+ (isFixed ? " fixed" :" goUp")}>
       <div className="hero__header container">
         <picture className="logo-div">
           <img src="./images/logo.png" />
