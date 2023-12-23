5f5b49ddde49b94ad0da04fb95f50faba6006128
james <jamesazzi193@gmail.com>
2023-12-23 17:18:04 +0200

    Fixing about page

--- src/components/about/about-us.tsx
+++ src/components/about/about-us.tsx
@@ -32,127 +32,142 @@ const About = () => {
 
   return (
     <section className="about-us-block">
-      <div className="about-us-container text-justify">
+      <div className="about-heading-container">
         <h1 className="about__title" data-aos="zoom-out">
           Welcome to Our NGO!
         </h1>
+      </div>
 
-        <div className="about-block">
-          <header className="block__header about__header">
-            <h1 className="block__heading about__heading" data-aos="fade-down">
-              <span className="number-badge number-badge--primary">1</span>
-              <p>{msg1}</p>
-            </h1>
-          </header>
-          <div className="about__body">
-            <p className="about__text" data-aos="fade-left">
-              We are a non-governmental organization (NGO) dedicated to
-              empowering Lebanese youth by providing free training sessions. Our
-              goal is to equip them with the skills needed to respond to the
-              needs of industrialists and employers, ultimately helping them
-              find suitable jobs.
-            </p>
-            <div className="about__icon">
-              <span>
-                <FaIdCard style={style} />
-              </span>
+      <div className="about-us-container text-justify">
+        <div className="about__display">
+          <div className="about-block">
+            <header className="block__header about__header">
+              <h1
+                className="block__heading about__heading"
+                data-aos="fade-down"
+              >
+                <span className="number-badge number-badge--primary">1</span>
+                <p>{msg1}</p>
+              </h1>
+            </header>
+            <div className="about__body">
+              <p className="about__text" data-aos="fade-left">
+                We are a non-governmental organization (NGO) dedicated to
+                empowering Lebanese youth by providing free training sessions.
+                Our goal is to equip them with the skills needed to respond to
+                the needs of industrialists and employers, ultimately helping
+                them find suitable jobs.
+              </p>
+              <div className="about__icon">
+                <span>
+                  <FaIdCard style={style} />
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="about__images">
-          <span className="scroller clickable" onClick={decrementIndex}>
-            &lt;
-          </span>
-          <div className="about__list">
-            {pics.map((aw, ind) => {
-              let cls = "next";
-              if (ind === index) cls = "active";
-              else if (
-                ind === index - 1 ||
-                (index === 0 && ind === pics.length - 1)
-              )
-                cls = "prev";
+          <div className="about__images">
+            <span className="scroller clickable" onClick={decrementIndex}>
+              &lt;
+            </span>
+            <div className="about__list">
+              {pics.map((aw, ind) => {
+                let cls = "next";
+                if (ind === index) cls = "active";
+                else if (
+                  ind === index - 1 ||
+                  (index === 0 && ind === pics.length - 1)
+                )
+                  cls = "prev";
 
-              return (
-                <picture className={"about__img " + cls}>
-                  <source type="./jpg" srcSet={`${aw.src} 1x`} />
-                  <img src={aw.src} alt="" />
-                </picture>
-              );
-            })}
+                return (
+                  <picture className={"about__img " + cls}>
+                    <source type="./jpg" srcSet={`${aw.src} 1x`} />
+                    <img src={aw.src} alt="" />
+                  </picture>
+                );
+              })}
+            </div>
+            <span className="scroller clickable" onClick={incrementIndex}>
+              &gt;
+            </span>
           </div>
-          <span className="scroller clickable" onClick={incrementIndex}>
-            &gt;
-          </span>
         </div>
 
-        <div className="about-block">
-          <header className="block__header about__header">
-            <h1 className="block__heading about__heading" data-aos="fade-down">
-              <span className="number-badge number-badge--primary">2</span>
-              <p>{msg2}</p>
-            </h1>
-          </header>
-          <div className="about__body">
-            <p className="about__text" data-aos="fade-left">
-              In addition to carrying out the necessary studies and statistics
-              to determine the employers' needs for efficient manpower, we aim
-              at providing trainees with the practical, linguistic, digital,
-              safety and life skills required by employers. By organizing and
-              implementing various social, health, sports and folk activities,
-              we assist the society to ensure its proper living in a clean,
-              healthy and safe environment free from all forms of pollution.
-            </p>
-            <div className="about__icon">
-              <span>
-                <FaBullseye style={style} />
-              </span>
+        <div className="about__display">
+          <div className="about-block">
+            <header className="block__header about__header">
+              <h1
+                className="block__heading about__heading"
+                data-aos="fade-down"
+              >
+                <span className="number-badge number-badge--primary">2</span>
+                <p>{msg2}</p>
+              </h1>
+            </header>
+            <div className="about__body">
+              <p className="about__text" data-aos="fade-left">
+                In addition to carrying out the necessary studies and statistics
+                to determine the employers' needs for efficient manpower, we aim
+                at providing trainees with the practical, linguistic, digital,
+                safety and life skills required by employers. By organizing and
+                implementing various social, health, sports and folk activities,
+                we assist the society to ensure its proper living in a clean,
+                healthy and safe environment free from all forms of pollution.
+              </p>
+              <div className="about__icon">
+                <span>
+                  <FaBullseye style={style} />
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="about-block">
-          <header className="block__header about__header">
-            <h1 className="block__heading about__heading" data-aos="fade-down">
-              <span className="number-badge number-badge--primary">3</span>
-              <p>{msg3}</p>
-            </h1>
-          </header>
-          <div className="about__body">
-            <ul className="goals-list">
-              <li data-aos="fade-left">
-                <span className="checkmark">✓</span>
-                <b>Free</b> trainings for Lebanese youth, in order to be able to
-                secure their future.
-              </li>
-              <li data-aos="fade-right">
-                <span className="checkmark">✓</span>
-                Full <b>cooperation</b> with employers to determine their needs
-                and develop the training courses’ curricula accordingly.
-              </li>
-              <li data-aos="fade-left">
-                <span className="checkmark">✓</span>
-                <b>Compentent</b> trainers who are known for their knowledge and
-                practical experience.
-              </li>
-              <li data-aos="fade-right">
-                <span className="checkmark">✓</span>
-                <b>Life</b> skills, work organization, personal and public
-                safety, providing first aids in addition to the purely technical
-                topics.
-              </li>
-              <li data-aos="fade-left">
-                <span className="checkmark">✓</span>
-                <b>Transportation</b> allowance for each training day
-                implemented in the Association’s premises.
-              </li>
-              <li data-aos="fade-right">
-                <span className="checkmark">✓</span>
-                <b>Certifcate</b> with a mention with what the course included
-                to each trainee who finishes the training.
-              </li>
-            </ul>
+          <div className="about-block">
+            <header className="block__header about__header">
+              <h1
+                className="block__heading about__heading"
+                data-aos="fade-down"
+              >
+                <span className="number-badge number-badge--primary">3</span>
+                <p>{msg3}</p>
+              </h1>
+            </header>
+            <div className="about__body">
+              <ul className="goals-list">
+                <li data-aos="fade-left">
+                  <span className="checkmark">✓</span>
+                  <b>Free</b> trainings for Lebanese youth, in order to be able
+                  to secure their future.
+                </li>
+                <li data-aos="fade-right">
+                  <span className="checkmark">✓</span>
+                  Full <b>cooperation</b> with employers to determine their
+                  needs and develop the training courses’ curricula accordingly.
+                </li>
+                <li data-aos="fade-left">
+                  <span className="checkmark">✓</span>
+                  <b>Compentent</b> trainers who are known for their knowledge
+                  and practical experience.
+                </li>
+                <li data-aos="fade-right">
+                  <span className="checkmark">✓</span>
+                  <b>Life</b> skills, work organization, personal and public
+                  safety, providing first aids in addition to the purely
+                  technical topics.
+                </li>
+                <li>
+                  <span className="checkmark">✓</span>
+                  <b>Transportation</b> allowance for each training day
+                  implemented in the Association’s premises.
+                </li>
+                <li>
+                  <span className="checkmark">✓</span>
+                  <b>Certifcate</b> with a mention with what the course included
+                  to each trainee who finishes the training.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
